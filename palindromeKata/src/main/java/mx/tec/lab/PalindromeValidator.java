7bf7afaea29a7227842ad5caf704b871b0cdb94d
Scyruz <a01561869@itesm.mx>
2021-04-16 16:30:57 -0700

    Step 5

--- palindromeKata/src/main/java/mx/tec/lab/PalindromeValidator.java
+++ palindromeKata/src/main/java/mx/tec/lab/PalindromeValidator.java
@@ -3,13 +3,13 @@ package mx.tec.lab;
 public class PalindromeValidator {
 
 	public boolean validate(String string) {
-    for ( int i = 0; i < string.length() / 2; i++) 
-    {
-        if(string.charAt(i) != string.charAt(string.length() - 1 - i)) 
-        	{
-            return false;
-            }
-        }
-        return true;
-    }
+		String clearString = string.replaceAll("\\s+","");
+		for(int i = 0; i < clearString.length()/2; i++) {
+			if(clearString.charAt(i) != clearString.charAt(clearString.length()-1-i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 }
--- palindromeKata/src/test/java/mx/tec/lab/PalindromeValidatorTest.java
+++ palindromeKata/src/test/java/mx/tec/lab/PalindromeValidatorTest.java
@@ -82,7 +82,43 @@ public class PalindromeValidatorTest {
 	        // Then
 	        boolean expectedResult = false;
 	        assertEquals(expectedResult, actualResult);
+	    }
+
+	 @Test
+	    void Given_APalindromeString_When_ValidatingPalindrome_Then_False() {
+	        // Given
+	        String string = "amba";
+	        // When
+	        boolean actualResult = validator.validate(string);
+
+	        // Then
+	        boolean expectedResult = false;
+	        assertEquals(expectedResult, actualResult);
 
 	    }
+	 @Test
+		void Given_APalindromePhraseString_When_ValidatingPalindrome_Then_True() {
+			//Given
+			String string = "a man a plan a canal panama";
+			//When
+			boolean actualResult = validator.validate(string);
+			//Then
+			boolean expectedResult = true;
+			
+			assertEquals(expectedResult, actualResult);
+		}
+		
+	@Test
+		void Given_ANotPalindromePhraseString_When_ValidatingPalindrome_Then_False() {
+			//Given
+			String string = "b man a plan a canal panama";
+			//When
+			boolean actualResult = validator.validate(string);
+			//Then
+			boolean expectedResult = false;
+			
+			assertEquals(expectedResult, actualResult);
+		}
+
 
 }
